08c439f73e7f76804d3cd9a2fde57792f31b28da
Adrien Taudière <adrien.taudiere@zaclys.net>
2024-10-03 15:13:11 +0200

    Continue to build the structure

--- _targets.R
+++ _targets.R
@@ -1,2 +1,29 @@
+source("R/functions.R")
+library("conflicted")
+library("MiscMetabar")
 library("targets")
 library("tarchetypes")
+
+here::i_am("_targets.R")
+
+tar_plan(
+  tar_target(
+    name = file_sam_data_csv,
+    command = "data/data_raw/metadata/sam_data.csv",
+    format = "file"
+  ),
+
+  tar_target(
+    name = file_refseq_taxo,
+    command = "data/data_raw/refseq/XXX",
+    format = "file"
+  ),
+
+  tar_target(
+    name = file_refseq_taxo,
+    command = "data/data_raw/refseq/XXX",
+    format = "file"
+  ),
+
+
+)
